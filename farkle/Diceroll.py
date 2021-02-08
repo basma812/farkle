11435a151e68163f4ac79caf211c48000b7b26d2
KarlHolmstrom <karl_holmstrom@live.se>
2021-02-08 14:34:22 +0100

    tärningskast

---
+++ farkle/Diceroll.py
@@ -0,0 +1,8 @@
+import random
+# list of saved dices from previous throw
+def diceroll(savedlist):
+    d = 6-len(savedlist)
+    diceroll = []
+    print("You are throwing {0} dices".format(d))
+    for i in range(d):
+        diceroll.append(random.randint(1, 6))
