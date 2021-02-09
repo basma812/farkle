533dc57f99a9c2d317bea478c9103448d7ad6926
Samuel Karlström <sam.karlstrm@gmail.com>
2021-02-09 14:05:42 +0100

    var tvungen att göra en loop för att testa

--- farkle/bankmodule.py
+++ farkle/bankmodule.py
@@ -1,3 +1,4 @@
+# creates dictionary with players names and starting values 0
 def create_bank():
     bank = {}
     players = int(input("How many players are playing?[2-6]: "))
@@ -7,13 +8,14 @@ def create_bank():
     return bank
 
 
+# prints all players with their values in a table
 def print_bank(bank):
     print("|", end="")
     for player in bank.keys():
         print(f" {player}\t|", end="")
     print()
     for player in bank.keys():
-        print("----------", end="")
+        print("--------", end="")
     print()
     print("|", end="")
     for points in bank.values():
@@ -22,3 +24,20 @@ def print_bank(bank):
     print("|", end="")
     for points in bank.values():
         print("    \t|", end="")
+    print()
+
+
+# checks if any player have over 10000 points,
+def check_if_over_limit(player, value):
+    if value >= 10000:
+        print(f"{player} has reached 10000 points! Last round begins!")
+        return True
+    else:
+        return False
+
+
+# checks and prints final Winner
+def print_winner(bank):
+    w = max(bank, key=bank.get)
+    print("And the winner iiiiiisss....!!!!")
+    print(w + " !!!  Congratz")
--- farkle/farkle.py
+++ farkle/farkle.py
@@ -1,14 +1,34 @@
 # import random
 import bankmodule as b
 
-
-# welcome_msg
 bank = b.create_bank()
 b.print_bank(bank)
+play = True
+# list of players over 10000
+winning_players = []
+# welcome_msg
 
 
 # def play(dice, players):
+while play:
+    for player, value in bank.items():
+        if winning_players:
+            print(f"{winning_players[0]} is winning")
+            # one last round has been played (player is same as first in list)
+            if winning_players[0] == player:
+                play = False
+                break
+        points = int(input(f"how many points did {player} gain: "))
+        bank[player] += points
+        # every round - check winner
+        if bank[player] >= 10000:
+            winning_players.append(player)
+            print(f"{player} has reached 10000 points! Last round begins!")
 
+    b.print_bank(bank)
+
+
+b.print_winner(bank)
 
 # def roll_dice(dice, players):
 
