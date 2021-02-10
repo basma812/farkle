309b26b1594a802a24cbd3f59dc7061b19e3f54c
Ludvig Viveland <ludvigvive@gmail.com>
2021-02-10 11:41:27 +0100

    check_straight done

---
+++ farkle/check_straight.py
@@ -0,0 +1,13 @@
+#checks if its a straight from 1-6
+def check_straight(l):
+    l.sort()
+    if len(l) != 6:
+        return False
+    if l[0] != l[1]:
+        if l[1] != l[2]:
+            if l[2] != l[3]:
+                if l[3] != l[4]:
+                    if l[4] != l[5]:
+                        return True
+                    
+    return False
