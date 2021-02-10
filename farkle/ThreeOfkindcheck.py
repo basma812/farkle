101ebcc91ef75d4c142076c5770a52dde24802c5
LordRatcatcher <sundin.albin@hotmail.com>
2021-02-10 14:12:57 +0100

    Return complete?

--- farkle/ThreeOfkindcheck.py
+++ farkle/ThreeOfkindcheck.py
@@ -10,6 +10,8 @@ def trissCheck(dice_List):
       
     if dice_List[0] == dice_List[2] or dice_List[1] == dice_List[3] or dice_List[2] == dice_List[4] or dice_List[3] == dice_List[5]:
         threeOfkind = 1
+    else:
+        return False
     if threeOfkind == 1:         
         if dice_List[0] == dice_List[2] and dice_List[0] == 1 or dice_List[1] == dice_List[3] and dice_List[1] == 1 or dice_List[2] == dice_List[4]  and dice_List[4] == 1 or dice_List[3] == dice_List[5] and dice_List[5] == 1: 
             tempPlayerpoints += 1000
@@ -33,7 +35,9 @@ def trissCheck(dice_List):
             tempPlayerpoints = 0
             tempPlayerpoints = 2500
             playerPointstogive += tempPlayerpoints
-    print(playerPointstogive)   
-
+            tempPlayerpointsint = int(tempPlayerpoints)
+        return True
+        return tempPlayerpointsint
+        
            
     
